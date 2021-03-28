9d311f083b49e031032815da4bc1a7bea202dace
David Wheatley <hi@davwheat.dev>
2021-03-28 05:49:07 +0100

    Add API JSDoc blocks

--- src/v1/compat-latest/index.ts
+++ src/v1/compat-latest/index.ts
@@ -12,6 +12,51 @@ export function SetUpEndpoints(app: express.Express) {
     respondWithError(res, 400, `Invalid syntax. Please see the documentation at: ${process.env.BASE_URL}/v1`)
   })
 
+  /**
+   * @api {get} /v1/compat-latest/:packageName Supports latest Flarum core version
+   *
+   * @apiName CompatLatest
+   * @apiDescription
+   *   Fetches a badge that shows whether the provided extension is compatible with the latest version of `flarum/core`.
+   *
+   *   Uses the information provided by the [Extiverse](https://extiverse.com/) API, kindly hosted free-of-charge by [Daniël Klabbers (luceos)](https://github.com/luceos).
+   *
+   * @apiGroup Badges
+   * @apiVersion 1.0.0
+   *
+   * @apiParam (URL parameters) {string} packageName The extension's package name.
+   *
+   * @apiParamExample {string} packageName
+   *   "fof/formatting"
+   *
+   * @apiSuccessExample {svg} Support
+   *   <svg
+   *     xmlns="http://www.w3.org/2000/svg"
+   *     xmlns:xlink="http://www.w3.org/1999/xlink" width="183" height="20" role="img" aria-label="Supports latest Flarum: Yes">
+   *     <title>Supports latest Flarum: Yes</title>
+   *     <!-- ... -->
+   *   </svg>
+   *
+   * @apiSuccessExample {svg} No support
+   *   <svg
+   *     xmlns="http://www.w3.org/2000/svg"
+   *     xmlns:xlink="http://www.w3.org/1999/xlink" width="183" height="20" role="img" aria-label="Supports latest Flarum: No">
+   *     <title>Supports latest Flarum: No</title>
+   *     <!-- ... -->
+   *   </svg>
+   *
+   * @apiErrorExample {text} 404 Not Found
+   *   Not found
+   *
+   *   No results found for "flarum/nuke-discourse". Make sure this matches your composer.json's name field.
+   *
+   * @apiErrorExample {text} 500 Upstream Extiverse API error
+   *   Unknown upstream API error
+   *
+   *   An unknown error occurred with the upstream API used to fetch extension data. Please try again later, or contact @MrJeeves#6969 on Discord.
+   *
+   * @apiSampleRequest v1/compat-latest/fof/formatting
+   */
   app.get('/v1/compat-latest/*', async (req, res) => {
     const packageName = req.params[0]
 
--- src/v1/ext-icon/index.ts
+++ src/v1/ext-icon/index.ts
@@ -11,6 +11,38 @@ export function SetUpEndpoints(app: express.Express) {
     respondWithError(res, 400, `Invalid syntax. Please see the documentation at: ${process.env.BASE_URL}/v1`)
   })
 
+  /**
+   * @api {get} /v1/ext-icon/:packageName Get extension icon
+   *
+   * @apiName ExtIcon
+   * @apiDescription
+   *   Returns a specified extension's icon which would be shown on the Flarum admin dashboard.
+   *
+   *   Uses the information provided by the [Extiverse](https://extiverse.com/) API, kindly hosted free-of-charge by [Daniël Klabbers (luceos)](https://github.com/luceos).
+   *
+   * @apiGroup Extension info
+   * @apiVersion 1.0.0
+   *
+   * @apiParam (URL parameters) {string} packageName The extension's package name.
+   *
+   * @apiParamExample {string} packageName
+   *   "fof/formatting"
+   *
+   * @apiSuccessExample {text} Extension icon
+   *   raw PNG data
+   *
+   * @apiErrorExample {text} 404 Not Found
+   *   Not found
+   *
+   *   No results found for "flarum/nuke-discourse". Make sure this matches your composer.json's name field.
+   *
+   * @apiErrorExample {text} 500 Upstream Extiverse API error
+   *   Unknown upstream API error
+   *
+   *   An unknown error occurred with the upstream API used to fetch extension data. Please try again later, or contact @MrJeeves#6969 on Discord.
+   *
+   * @apiSampleRequest /v1/ext-icon/fof/formatting
+   */
   app.get('/v1/ext-icon/*', async (req, res) => {
     const packageName = req.params[0]
 
