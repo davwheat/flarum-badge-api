b35bf40e95a00097c968502d1d94aec828976d74
David Wheatley <hi@davwheat.dev>
2021-03-28 05:51:07 +0100

    Add apiDoc to project

---
+++ apidoc.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'Flarum Badge API',
+  version: '1.0.0',
+  description: 'A simple and free API for everything you need related to Flarum extensions.',
+  apidoc: {
+    title: 'Flarum Badge API',
+    base_url: 'https://flarum-badge-api.davwheat.dev/',
+  },
+  details: [
+    'This API is meant for simple use in public-facing applications for ease of use. This is not intended to be used in a client-server application: instead you should implement the Extiverse API yourself.',
+    'This service offers no uptime guarantee, and may suffer downtime at any moment, for any reason. The service will remain up for as long as feasible.',
+  ],
+}
