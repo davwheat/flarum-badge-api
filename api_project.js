218faae79dfb23c8e9af80e47a5529713fa08767
davwheat <hi@davwheat.dev>
2021-03-28 05:15:08 +0000

    Deploying to prod from @ davwheat/flarum-badge-api@cbe2c287cfe470c0ee8542efa7312aebcde44f08 🚀

--- api_project.js
+++ api_project.js
@@ -2,16 +2,24 @@ define({
   "name": "Flarum Badge API",
   "version": "1.0.0",
   "description": "A simple and free API for everything you need related to Flarum extensions.",
+  "title": "Docs - Flarum Badge API",
   "apidoc": "0.3.0",
   "details": [
     "This API is meant for simple use in public-facing applications for ease of use. This is not intended to be used in a client-server application: instead you should implement the Extiverse API yourself.",
     "This service offers no uptime guarantee, and may suffer downtime at any moment, for any reason. The service will remain up for as long as feasible."
   ],
+  "vcs": {
+    "url": "https://github.com/davwheat/flarum-badge-api",
+    "service": "GitHub"
+  },
+  "template": {
+    "withGenerator": false
+  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-28T05:07:51.917Z",
+    "time": "2021-03-28T05:15:05.973Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
